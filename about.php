56e61f7ae25ffd76cf333be6add837bd296cde4c
snowindy <blackorangebox@gmail.com>
2011-10-27 11:53:04 +0400

    refreshed info

--- about.php
+++ about.php
@@ -5,23 +5,20 @@ include("header.php");
 <!-- основной блок -->
 
 	<div id="content">
-	<table width="800" border="0" cellpadding="10" cellspacing="1">
-  <tr>
-    <td width="400"><h2></a>О нас</h2></td>
-    <td width="400"><h3>Мастера</h3></td>
-    
-  </tr>
-  <tr>
-    <td width="400" >
-      <img src="img/img_11.jpg" width="400" height="31" alt="вот так">
+	<div class="content-cell">
+	<h2></a>О нас</h2>
+	 <img src="img/img_11.jpg" width="400" height="31" alt="вот так">
 <h3>Кто.</h3>
     <p> Молодые люди. Выпускники московских вузов (технических и художественных). </p>
     <p>&nbsp;</p>
 	<h3>Зачем.</h3>
 	<p class="meta">&lt;!--За шкафом.--&gt;</p>
 	<p></p>
-    <p>&nbsp;</p></td>
-    <td width="400"><img src="img/img_11.jpg" width="400" height="31" alt="вот так">
+    <p>&nbsp;</p>
+	</div>
+	<div class="content-cell">
+	<h3>Мастера</h3>
+	<img src="img/img_11.jpg" width="400" height="31" alt="вот так">
       <h3>Арт-директор.</h3>
 	  <h3>Дизайнер.</h3>
 	  <p>Я дизайнер</p>
@@ -30,12 +27,8 @@ include("header.php");
 	  <h3>Флорист.</h3>
 	  <p>Я флорист</p>
 	  <h3>Фоторгаф.</h3>
-	  
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p></td>
-     </tr>
-</table>
+	</div>
+	
 </div>
 <!-- конец основного блока -->
 
