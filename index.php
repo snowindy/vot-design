00c9f469bea23c8d7d6bd83fd8a9dd12f4133033
snowindy <blackorangebox@gmail.com>
2011-11-06 18:45:17 +0300

    fixed opera bug

--- index.php
+++ index.php
@@ -6,9 +6,7 @@ include("header.php");
 <!-- основной блок -->
 
 <div id="content">
-    <div class="content-cell">
-        <a class="invis-anch" id="home" name="home"><div></div></a>
-
+    <div class="content-cell"  id="home">
         <h2>Дизайн-студия</h2>
 
         <img src="img/img_10.jpg" width="400" height="215" alt="вот так"/>
@@ -18,9 +16,7 @@ include("header.php");
         <p class="meta">&lt;!-- Если не нравится, то вам не к нам. --&gt;</p>
         <p>&nbsp;</p>
     </div>
-    <div class="content-cell">
-        <a class="invis-anch" id="service" name="service"><div></div></a>
-
+    <div class="content-cell" id="service">
         <h2>Услуги</h2>
 
         <img src="img/img_13.jpg" width="400" height="215" alt="вот так"/>
@@ -58,8 +54,7 @@ include("header.php");
   <p>&nbsp;</p>-->
 
     </div>
-    <div class="content-cell has-text">
-        <a class="invis-anch" id="contacts" name="contacts"><div></div></a>
+    <div class="content-cell has-text" id="contacts">
 
         <h2>Контакты</h2>
 
@@ -68,8 +63,7 @@ include("header.php");
         <p><strong>e-mail:</strong> info@vot-design.com</p>
         <p><strong>tel.:</strong> 8 (903) 216-38-45</p>
     </div>
-    <div class="content-cell has-text">
-        <a class="invis-anch" id="portfolio" name="portfolio"><div></div></a>
+    <div class="content-cell has-text" id="portfolio">
 
         <h2><a href="portfolio.php" title="Portfolio">Портфолио</a></h2>
 
